fa5538c160e0b73ed215fa440bb5a1c60cfd6d88
Xberto Zenteno <azenteno@laradio.cl>
2016-12-11 08:29:12 -0300

    -Comunicacion con el servidor -Ordenamiento del código -Falta obtener el ultimo ID sincronizado (ver con sqlite)

--- PreServicio/Access.cs
+++ PreServicio/Access.cs
@@ -4,6 +4,7 @@ using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 
 namespace PreServicio
 {
@@ -21,5 +22,78 @@ namespace PreServicio
             reader = command.ExecuteReader();
             return reader;       
         }
+
+        public string usuarios(string last_id)
+        {
+            string[] apellido = null;
+            string last = "";
+            List<Dictionary<string,string>> valores = new List<Dictionary<string,string>>();
+            try
+            {
+                var reader = query("SELECT USERID, NAME, TITLE from  userinfo");
+                while (reader.Read())
+                {
+                    if ((apellido = reader[1].ToString().Split(' ')).Length > 1)
+                        last = apellido[1];
+                    else
+                        last = "";
+                    valores.Add(new Dictionary<string, string>
+                    {
+                        {"reloj_id", reader[0].ToString()},
+                        {"nombre", apellido[0]},
+                        {"apellido", last},
+                        {"cargo", reader[2].ToString()}
+                            
+                    });
+                }
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                string json = js.Serialize(valores);
+                return json;
+            }
+            catch (Exception e) { return ""; }
+        }
+
+        public string check(string last)
+        {
+            List<Dictionary<string, string>> valores = new List<Dictionary<string, string>>();
+            try
+            {
+                var reader = query("SELECT USERID, CHECKTIME, CHECKTYPE from  userinfo");
+                while (reader.Read())
+                {
+                    valores.Add(new Dictionary<string, string>
+                    {
+                        {"reloj_id", reader[0].ToString()},
+                        {"checktime", reader[1].ToString()},
+                        {"checktype", reader[2].ToString()}
+                    });
+                }
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                string json = js.Serialize(valores);
+                return json;
+            }
+            catch (Exception e) { return ""; }
+        }
+
+        public string del(string last)
+        {
+            List<Dictionary<string, string>> valores = new List<Dictionary<string, string>>();
+            try
+            {
+                var reader = query("SELECT USERID FROM SERVERLOG WHERE EVENT='user delete'");
+                while (reader.Read())
+                {
+                    valores.Add(new Dictionary<string, string>
+                    {
+                        {"reloj_id", reader[0].ToString()}
+                    });
+                }
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                string json = js.Serialize(valores);
+                return json;
+            }
+            catch (Exception e) { return ""; }
+        }
+
     }
 }
--- PreServicio/Program.cs
+++ PreServicio/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Configuration;
 using System.Data.OleDb;
 using System.Data.SQLite;
@@ -16,57 +17,57 @@ namespace PreServicio
     {
         private static string db;
         private static string connString;
+        private static string add = ConfigurationManager.AppSettings["add"].ToString();
+        private static string del = ConfigurationManager.AppSettings["del"].ToString();
+        private static string check = ConfigurationManager.AppSettings["check"].ToString();
+        private static string json;
         static void Main(string[] args)
         {
             db = ConfigurationManager.AppSettings["database"].ToString();
             connString = string.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0}",db);
-            HttpWebRequest http = (HttpWebRequest)WebRequest.Create("http://pagina.com/webservice/getLastCheck");
             List<Dictionary<string,string>> valores = new List<Dictionary<string,string>>();
+            Access adb = new Access(connString);
             try {
-                using (OleDbConnection connection = new OleDbConnection(connString))
-                {
-                    connection.Open();
-                    OleDbDataReader reader = null;
-                    OleDbCommand command = new OleDbCommand("SELECT USERID, NAME, TITLE from  userinfo", connection);
-                    reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        valores.Add(new Dictionary<string, string>
-                        {
-                            {"reloj_id", reader[0].ToString()},
-                            {"nombre", reader[1].ToString()},
-                            {"cargo", reader[2].ToString()}
-                            
-                        });
-                    }
-                    JavaScriptSerializer js = new JavaScriptSerializer();
-                    string json = js.Serialize(valores);
-                    Console.WriteLine(json);
-                    /*if (!string.IsNullOrEmpty(json)) {
-                        var request = (HttpWebRequest)WebRequest.Create("http://192.168.0.101:8080");
-                        request.Method = "POST";
-                        var requestBody = Encoding.UTF8.GetBytes(json);
-                        request.ContentLength = requestBody.Length;
-                        request.ContentType = "application/json";
-                        using (var requestStream = request.GetRequestStream())
-                        {
-                            requestStream.Write(requestBody, 0, requestBody.Length);
-                        }
-                        using (var respuesta = request.GetResponse())
-                        {
-                            using (var leeRespuesta = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(1252)))
-                            {
-                                var output = leeRespuesta.ReadToEnd();
-                            }
-                        }
-                        
-                    }*/
-                }
+               if (!string.IsNullOrEmpty((json = adb.usuarios(""))))
+               {
+                   post(add, json);
+               }
+               if (!string.IsNullOrEmpty((json = adb.check(""))))
+               {
+                   post(check, json);
+               }
+               if (!string.IsNullOrEmpty((json = adb.del(""))))
+               {
+                   post(del, json);
+               }
                 
                     
             }catch(Exception e){
                 Console.WriteLine(e.ToString());
             }
+
+        }
+
+        public static void post(string url, string json){
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+
+                //Console.WriteLine(result);
+            }
+
 
         }
     }
